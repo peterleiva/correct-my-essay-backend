e513097a04dcdc88a20a4a92e80851e1c484eee4
Peter Leiva <pleiva@id.uff.br>
2020-07-28 12:49:00 -0300

    Add unit test for user
    Test user valid or invalid credentials behaviour

---
+++ src/test/model/user.spec.ts
@@ -0,0 +1,44 @@
+import UserModel, { UserDocument } from '../../security/user';
+
+describe('User', () => {
+	// describe('Validations', () => {
+	// 	it('is invalid with no firstname, lastname, ....');
+	// });
+
+	describe('With Credential', () => {
+		let user: UserDocument;
+
+		beforeEach(() => {
+			user = new UserModel({ credential: {} });
+		});
+
+		describe('Methods', () => {
+			describe('.authorized', () => {
+				const invalidPassword = '829293';
+				const validPassword = '123456';
+
+				beforeEach(async () => {
+					await user.credential.generateHash(validPassword);
+				});
+
+				it('Authorize with valid credentials', async () => {
+					expect(await user.credential.authorize(validPassword)).toBe(true);
+				});
+
+				it('Do not authorize with invalid credentials', async () => {
+					expect(await user.credential.authorize(invalidPassword)).toBe(false);
+				});
+			});
+
+			describe('.generateHash', () => {
+				const password = '8123823';
+
+				it('Fills the password hash field', async () => {
+					await user.credential.generateHash(password);
+					expect(user.credential.passwordHash).toBeTruthy();
+				});
+			});
+		});
+	});
+});
+
