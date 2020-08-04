84e1bef64d2e53be38005de1d18db19b11e5f7b7
Peter Leiva <pleiva@id.uff.br>
2020-08-04 08:05:54 -0300

    Configure jest to run tsconfig.test.json

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,21 @@
 module.exports = {
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.test.json',
+		},
+	},
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	coverageDirectory: 'coverage',
 	collectCoverage: true,
 	notify: true,
+	moduleDirectories: ['node_modules', 'src', 'test'],
+	moduleFileExtensions: ['ts', 'js'],
+	moduleNameMapper: {
+		'^src/(.*)$': '<rootDir>/src/$1',
+		'^test/(.*)$': '<rootDir>/test/$1',
+	},
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest',
+	},
 };
