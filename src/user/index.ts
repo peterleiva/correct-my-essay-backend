b10dd2bc9798e8fa81b1a4f82b2648101cb11724
Peter Leiva <pleiva@id.uff.br>
2020-08-05 22:31:56 -0300

    Add findbyemail static method to users
    Also, restructure the model export to use the static member

---
+++ src/user/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview User module index
+ */
+
+import { User, UserDocument } from './user';
+
+
+export { User, UserDocument };
--- src/user/user.ts
+++ src/user/user.ts
@@ -2,13 +2,25 @@
  * @fileoverview This file contain all user related model using mongoose
  */
 
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document, Model, Query } from 'mongoose';
 import Email from 'src/lib/mongoose/types/email';
 import { LoginCredentialSchema, LoginCredentialEmbedded }
 	from './login-credential';
 
 /**
- * Represents a application user with optional login credential
+ * User models which constructs user documents
+ */
+interface UserModel extends Model<UserDocument> {
+	/**
+	 * Find user documents by email
+	 *
+	 * @param {String} email
+	 */
+	findByEmail(email: string): Query<UserDocument>;
+}
+
+/**
+ * Represents a application user with and optional login credential
  *
  * A User has some identity attributes and a optional credential value for
  * user with password. User without password can be authenticated via
@@ -41,12 +53,15 @@ class User {
 }
 
 /**
- * Represents a object returned by mongoose model constructor
+ * User documents defines a object returned by User model
  *
- * @interface
+ * It has all behaviour from mongoose documents and is itself a user class
  */
-export interface UserDocument extends Document, User { }
+export type UserDocument = Document & User;
 
+/**
+ * User schema definition
+ */
 const schema: Schema<User> = new Schema({
 	firstName: {
 		type: String,
@@ -80,8 +95,21 @@ const schema: Schema<User> = new Schema({
 
 schema.loadClass(User);
 
-const UserModel = model<UserDocument>('User', schema);
+/**
+ * Lookup a unique user by email
+ *
+ * Uses mongoose model to query through findOne to retrivies a unique user by
+ * its email, returning a mongoose query
+ *
+ * @param {string} email email to be lookup
+ * @return {Query<UserDocument>}
+ */
+schema.statics.findByEmail = function(email: string): Query<UserDocument> {
+	return this.findOne({ email: email });
+};
 
-export { UserModel as User };
+const users: UserModel = model<UserDocument, UserModel>('User', schema);
 
-export default UserModel;
+export { users as User };
+
+export default users;
