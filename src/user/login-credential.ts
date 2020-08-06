fdacd6750ecf447b385ec2b18af9f37870f72858
Peter Leiva <pleiva@id.uff.br>
2020-08-06 01:17:14 -0300

    Does not generate user credential if there's no password

--- src/user/login-credential.ts
+++ src/user/login-credential.ts
@@ -84,6 +84,11 @@ LoginCredentialSchema.loadClass(LoginCredential);
 
 LoginCredentialSchema.pre<LoginCredentialDocument>('validate',
 	async function(this: LoginCredentialDocument) {
+		// eslint-disable-next-line no-invalid-this
+		if (!this.password) {
+			return;
+		}
+
 		try {
 			// eslint-disable-next-line no-invalid-this
 			await this.generateHash();
