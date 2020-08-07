cd8e6b816a027c9ddac241235e1fc111c98992af
Peter Leiva <pleiva@id.uff.br>
2020-08-07 20:13:21 -0300

    Add no-var-requires to eslint
    Some is written in pure javascript, so it is necessary to use require instead of import

--- src/lib/json-api/serializer.ts
+++ src/lib/json-api/serializer.ts
@@ -1,8 +1,7 @@
 /**
- * @fileoverview JsonApiSerializer base configuration
+ * @fileoverview JsonApiSerializer core jsonapi serializer
  */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const JSONAPISerializer = require('json-api-serializer');
 
 const Serializer = new JSONAPISerializer({
