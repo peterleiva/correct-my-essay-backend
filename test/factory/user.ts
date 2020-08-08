945914c1a8eff690e44ab002cd5205e678a0dfc2
Peter Leiva <pleiva@id.uff.br>
2020-08-08 04:21:25 -0300

    Install rosie for factory to build users

--- test/factory/user.ts
+++ test/factory/user.ts
@@ -2,5 +2,21 @@
  * @fileoverview User factory file
  */
 
-import UserDocument from 'src/security/user';
+import { Factory } from 'rosie';
+import * as faker from 'faker';
 
+export default new Factory()
+	.attrs({
+		firstName: faker.name.firstName(),
+		lastName: faker.name.lastName(),
+		email: faker.internet.email(),
+	})
+	.attr('active', false)
+	.option('password', false)
+	.option('withCredential', false)
+	.attr('credential', ['password', 'withCredential'],
+		(password, withCredential) => {
+			if (password) return { password: password };
+
+			return withCredential ? { password: faker.internet.password() } : null;
+		});
