c240c45d03f404dba251c1368e97de2d4a6e4951
Peter Leiva <pleiva@id.uff.br>
2020-08-09 00:05:36 -0300

    Test user delete feature and reestructure spec
    Use describe block to organize users in 3 main sections, retreving,
    deleting and creation. Also teste delete features

--- test/feature/user-management.spec.ts
+++ test/feature/user-management.spec.ts
@@ -9,72 +9,93 @@ import { auth } from '../lib/auth';
 describe('User Management', () => {
 	let user: UserDocument;
 	let accessToken: string;
-	let bearerToken = 'Bearer ';
+	let bearerToken: string;
+	const endpoint = '/users/';
 
 	databaseSetup();
 
 	beforeEach(async () => {
 		[user, accessToken] = await auth();
-		bearerToken += accessToken;
+		bearerToken = 'Bearer ' + accessToken;
 	});
 
-	afterEach(() => [user, accessToken, bearerToken] = [null, null, 'Bearer ']);
+	describe('Retreving', () => {
+		test('Get list with one user', async () => {
+			return request(app)
+				.get(endpoint)
+				.set('Authorization', bearerToken)
+				.expect(200,
+					JSON.stringify(await Serializer.serialize('users', [user])));
+		});
 
-	test('Get list with one user', async () => {
-		return request(app)
-			.get('/users')
-			.set('Authorization', bearerToken)
-			.expect(200,
-				JSON.stringify(await Serializer.serialize('users', [user])));
+		test('Get a list of multiple users', async () => {
+			const users = await User.create(Factory.buildList(4));
+			const serializedUsers = await Serializer
+				.serialize('users', [user, ...users]);
+
+			return request(app)
+				.get(endpoint)
+				.set('Authorization', bearerToken)
+				.expect(200, JSON.stringify(serializedUsers));
+		});
+
+		test.skip('Get inexistent user', async () => {
+			return request(app)
+				.get(endpoint + '10')
+				.set('Authorization', bearerToken)
+				.expect(404,
+					JSON.stringify(await Serializer.serialize('users', null)));
+		});
+
+		test('Get single user', async () => {
+			const user = await User.create(Factory.build());
+
+			return request(app)
+				.get(endpoint + user.id)
+				.set('Authorization', bearerToken)
+				.expect(200,
+					JSON.stringify(await Serializer.serialize('users', user)));
+		});
 	});
 
-	test('Get a list of multiple users', async () => {
-		const users = await User.create(Factory.buildList(4));
-		const serializedUsers = await Serializer
-			.serialize('users', [user, ...users]);
+	describe('Creating', () => {
+		test('Create a user successfully', async () => {
+			const user = Factory.build();
+			return request(app)
+				.post(endpoint)
+				.type('json')
+				.set('Authorization', bearerToken)
+				.send(user)
+				.expect(200);
+		});
 
-		return request(app)
-			.get('/users')
-			.set('Authorization', bearerToken)
-			.expect(200, JSON.stringify(serializedUsers));
+		test('Create a invalid user', async () => {
+			const user = Factory.build({ firstName: null });
+
+			return request(app)
+				.post(endpoint)
+				.type('json')
+				.set('Authorization', bearerToken)
+				.send(user)
+				.expect(404);
+		});
 	});
 
-	test('Create a user successfully', async () => {
-		const user = Factory.build();
-		return request(app)
-			.post('/users')
-			.type('json')
-			.set('Authorization', bearerToken)
-			.send(user)
-			.expect(200);
-	});
+	describe('Deleting', () => {
+		test('Existing User', async () => {
+			const user = await User.create(Factory.build());
 
-	test('Create a invalid user', async () => {
-		const user = Factory.build({ firstName: null });
+			return request(app)
+				.delete(endpoint + user.id)
+				.set('Authorization', bearerToken)
+				.expect(204);
+		});
 
-		return request(app)
-			.post('/users')
-			.type('json')
-			.set('Authorization', bearerToken)
-			.send(user)
-			.expect(404);
-	});
-
-	test.skip('Get inexistent user', async () => {
-		return request(app)
-			.get('/users/10')
-			.set('Authorization', bearerToken)
-			.expect(404,
-				JSON.stringify(await Serializer.serialize('users', null)));
-	});
-
-	test('Get single user', async () => {
-		const user = await User.create(Factory.build());
-
-		return request(app)
-			.get('/users/' + user.id)
-			.set('Authorization', bearerToken)
-			.expect(200,
-				JSON.stringify(await Serializer.serialize('users', user)));
+		test('Inexistent User', async () => {
+			return request(app)
+				.delete(endpoint + '100')
+				.set('Authorization', bearerToken)
+				.expect(404);
+		});
 	});
 });
