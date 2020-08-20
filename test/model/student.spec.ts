3fcf961402d4974aad3d071ec6ca00fb16ee2c31
Peter Leiva <pleiva@id.uff.br>
2020-08-20 16:02:48 -0300

    Move student.spec to test/model

--- test/model/student.spec.ts
+++ test/model/student.spec.ts
@@ -4,7 +4,8 @@
 
 import databaseSetup from 'test/lib/database-setup';
 import { StudentDocument, StudentModel } from 'src/user/student';
-import StudentFactory from '../../factory/student';
+import StudentFactory from '../factory/student';
+import { UserType } from 'src/user/user';
 
 describe('Student', () => {
 	let student: StudentDocument;
@@ -17,5 +18,12 @@ describe('Student', () => {
 
 	it('Create Successfuly', async () => {
 		await expect(student.save()).resolves.toEqual(student);
+	});
+
+	describe('Attributes', () => {
+		it('Has a __t attribute discriminator', async () => {
+			await student.save();
+			expect(student.__t).toEqual(UserType.Student);
+		});
 	});
 });
